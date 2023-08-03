5c380f4ebee83c241a06671fd4bc208e8c3a823b
Nathalia Nunes <nathalia-89@live.com>
2023-08-03 16:00:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("profile img")
+  // substituir a imagem
+  if(html.classList.contains("light")) {
+    // se tiver light mode, add a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
