d45e860c4c1f7ed147ab3acc9378dc7bfc92492c
Nathalia Nunes <nathalia-89@live.com>
2023-11-02 19:46:33 -0300

    mensagem

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function toggleMode() {
   html.classList.toggle("light")
 
   //pegar a tag img
-  const img = document.querySelector("profile img")
+  const img = document.querySelector("#profile-img")
+  const insta = document.querySelector("#profile-insta")
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, add a imagem light
-    img.setAttribute("src", "./assets/nathalia.jpge")
+    insta.textContent = "@nathalianunesbeauty"
+    img.setAttribute("src", "./assets/nathalia.jpeg")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/geo.jpge")
+    insta.textContent = "@geovanaraujo_sb"
+    img.setAttribute("src", "./assets/geo.jpeg")
   }
 }
